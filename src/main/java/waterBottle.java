d0f932288dc28189f3f41c3be516f4c7b2140a03
lukerhys <hello@lukemelvin.com>
2020-03-23 16:58:28 +0000

    hasVolume test passes

--- src/main/java/waterBottle.java
+++ src/main/java/waterBottle.java
@@ -1,10 +1,14 @@
 public class waterBottle {
 
     private int volume;
-    this.volume = 100;
 
-    public waterBottle(int volume){
-        this.volume = volume;
+    public waterBottle(){
+        this.volume = 100;
+    }
+
+
+    public int getVolume(){
+        return this.volume;
     }
 
 }
--- src/test/java/waterBottleTest.java
+++ src/test/java/waterBottleTest.java
@@ -16,31 +16,31 @@ public class waterBottleTest {
     public void hasVolume(){
         assertEquals(100, waterBottle.getVolume());
     }
-
-    @Test
-    public void hasOneDrink(){
-        assertEquals(90, waterBottle.drink());
-    }
-
-    @Test
-    public void hasThreeDrinks(){
-        waterBottle.drink();
-        waterBottle.drink();
-        waterBottle.drink();
-        assertEquals(70, waterBottle.getVolume());
-    }
-
-    @Test
-    public void hasEmptiedBottle(){
-        waterBottle.empty()
-        assertEquals(0, waterBottle.getVolume());
-    }
-
-    @Test
-    public void hasRefilledBottle(){
-        waterBottle.drink();
-        waterBottle.drink();
-        waterBottle.refill()
-        assertEquals(100, waterBottle.getVolume());
-    }
+//
+//    @Test
+//    public void hasOneDrink(){
+//        assertEquals(90, waterBottle.drink());
+//    }
+//
+//    @Test
+//    public void hasThreeDrinks(){
+//        waterBottle.drink();
+//        waterBottle.drink();
+//        waterBottle.drink();
+//        assertEquals(70, waterBottle.getVolume());
+//    }
+//
+//    @Test
+//    public void hasEmptiedBottle(){
+//        waterBottle.empty()
+//        assertEquals(0, waterBottle.getVolume());
+//    }
+//
+//    @Test
+//    public void hasRefilledBottle(){
+//        waterBottle.drink();
+//        waterBottle.drink();
+//        waterBottle.refill()
+//        assertEquals(100, waterBottle.getVolume());
+//    }
 }
