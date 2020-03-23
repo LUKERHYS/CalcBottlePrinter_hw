42ec4c6d4bf06e00ce9cb1a22971d6f6b3093aa6
lukerhys <hello@lukemelvin.com>
2020-03-23 17:24:36 +0000

    Added tests for a pass and failure print scenarios

--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -16,6 +16,15 @@ public class PrinterTest {
         assertEquals(250, printer.getSheets());
     }
 
+    @Test
+    public void numberOfSheetsHasReduced(){
+        printer.print(2, 2);
+        assertEquals(246, printer.getSheets());
+    }
 
-
+    @Test
+    public void failedToPrintDueToNoPaper(){
+        Printer lowPaperPrinter = new Printer(3);
+        assertEquals("Print Failure: Paper tray 1 Too Low", printer.print(2, 2));
+    }
 }
