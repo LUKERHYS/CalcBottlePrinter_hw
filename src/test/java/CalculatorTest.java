209f4369de0407b63b59112e6530b11df1c52bf3
lukerhys <hello@lukemelvin.com>
2020-03-23 15:47:43 +0000

    Created all calculator tests and all fail as expected

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
 
+    Calculator calc;
+
+    @Before
+    public void before(){
+        calc = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(6, calc.add(3, 3));
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(3, calc.subtract(3, 3));
+    }
+
+    @Test
+    public void canMultiply(){
+        assertEquals(10, calc.multiply(2, 5));
+    }
+
+    @Test
+    public void canDivide(){
+        assertEquals(2.5, calc.divide(5.0, 2.0));
+    }
 
 }
